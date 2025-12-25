8108b050bd4ba2bd559fa8038466de474e0d76ca
刘亚奇 <1904310303@qq.com>
2025-12-25 22:47:07 +0800

    项目搭建

---
+++ src/learn_data_structures.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void say_hello(){
+    std::cout << "Hello, from learn_data_structures!\n";
+}
---
+++ test/tHelloWorld.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(hello, hello1)
+{
+    EXPECT_EQ(1, 1);
+}
